cae80f60db38149baa1fc2454d1cabe7783ec11b
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-02-13 17:39:48 -0500

    Changer le facicon et cache la barre de navigation en mobile

--- src/Presentation.jsx
+++ src/Presentation.jsx
@@ -5,8 +5,8 @@ export default function Presentation(props){
         <section id="apropos" className="Presentation">
             <img src="images/autres/photo_profil.jpeg" alt="Photo de profil"/>
             <h1>Jérôme Trottier</h1>
-            <h5>Amoureux de la technologie depuis mon enfance, c'est avec intérêt que je suis cette passion dans mes études. Que ce soit de la programmation web, jeu ou du design, j'aime avoir à relever des défis pour davantage agrandir mes connaissances sur ces sujets. Toujours entrain d'apprendre, toujours entrain de m'améliorer.
-            </h5>
+            <h4>Amateur de technologie depuis mon enfance, c'est avec intérêt que je suis cette passion dans mes études. Que ce soit de la programmation web, jeu ou du design, j'aime avoir à relever des défis pour davantage agrandir mes connaissances sur ces sujets. Toujours en train d'apprendre, toujours en train de m'améliorer.
+            </h4>
             <a href="JeromeTrottier_CV2021.pdf" download>Télécharger CV</a>
         </section>
     );
