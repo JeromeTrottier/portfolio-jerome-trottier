6335a164c94d28129e8e364df5c936ec94a325be
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-03-02 20:58:21 -0500

    Update CV et modification de la presentation

--- src/Presentation.jsx
+++ src/Presentation.jsx
@@ -5,9 +5,9 @@ export default function Presentation(props){
         <section id="apropos" className="Presentation">
             <img src="images/autres/photo_profil.jpeg" alt="Photo de profil"/>
             <h1>Jérôme Trottier</h1>
-            <h4>Amateur de technologie depuis mon enfance, c'est avec intérêt que je suis cette passion dans mes études. Que ce soit de la programmation web, jeu ou du design, j'aime avoir à relever des défis pour davantage agrandir mes connaissances sur ces sujets. Toujours en train d'apprendre, toujours en train de m'améliorer.
+            <h4>Amateur de technologie depuis mon enfance, c'est avec intérêt que je poursuis cette passion dans mes études. Que ce soit de la programmation web, jeu ou du design, j'aime avoir à relever des défis pour agrandir davantage mes connaissances sur ces sujets. Toujours en train d'apprendre, toujours en train de m'améliorer.
             </h4>
-            <a href="Trottier_Jerome_CV.pdf" download>Télécharger CV</a>
+            <a href="JeromeTrottier_CV_2022.pdf" download>Télécharger CV</a>
         </section>
     );
 }
