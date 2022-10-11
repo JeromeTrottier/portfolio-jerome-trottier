b5da50830cd7174b0b2ad170ef35360b1cf24e22
Jérôme Trottier <jerome.trottier@hotmail.ca>
2022-10-11 14:02:15 -0400

    Correction faute de francais

--- src/Entete.jsx
+++ src/Entete.jsx
@@ -1,11 +1,11 @@
 import './sass/components/Entete.scss';
 import Navigation from './Navigation';
+import MenuBurger from './MenuBurger';
 
 export default function Entete(props){
     return (
         <header className="Entete">
-            <div className="contactLinks">
-            </div>
+            <div className="contactLinks"></div>
             <Navigation/>
             <div></div>
         </header>
